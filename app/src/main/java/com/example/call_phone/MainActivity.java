cd3b00d48a5b725329026b294fe018a08bc86a1f
chaimaa <c.louli@mundiapolis.ma>
2021-11-03 20:42:00 +0100

    Stable V

---
+++ app/src/main/java/com/example/call_phone/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.call_phone;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.text.BreakIterator;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText phone = findViewById(R.id.number);
+        final ImageButton call = findViewById(R.id.call);
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                callAtRuntime();
+            }
+        });
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void callAtRuntime() {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1);
+        } else {
+
+            BreakIterator phone = null;
+            String phoneNumber = phone.getText().toString();
+            Intent intent = new Intent(Intent.ACTION_CALL);
+
+            Object tel;
+            intent.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(intent);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                callAtRuntime();
+            } else {
+                Toast.makeText(this, "Oh No!!! Permission Denied. Try Again!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
